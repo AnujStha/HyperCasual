fd6965f2cf9d67fed4b1926c665eb40cabb5a1e2
AnujStha <anujshrestha.lubhoo@gmail.com>
2023-08-19 20:02:13 +0545

    created cream fill

--- Assets/Scripts/IceCreamCreator.cs
+++ Assets/Scripts/IceCreamCreator.cs
@@ -10,14 +10,19 @@ public class IceCreamCreator : MonoBehaviour
     [SerializeField] private float ringSpawnRate;
     [SerializeField] private float ringRadius;
     [SerializeField] private Transform createPosition;
+    [SerializeField] private Transform center;
     [SerializeField] private Material material;
     [SerializeField] private float uvMapSpacing;
     [SerializeField] private float creamMoveSpeed;
+    [SerializeField] private float landingPoint;
+    [SerializeField] private float fillRateY;
+    private float _currentY;
     private bool _create;
     private const float VerticesSqThreshold = .01f;
 
     private void Start()
     {
+        _currentY = landingPoint;
         _creator = StartCoroutine(Make());
     }
 
@@ -34,6 +39,20 @@ public class IceCreamCreator : MonoBehaviour
         Dictionary<int, Vector3> targetValues=new Dictionary<int, Vector3>();
         StartCoroutine(CreamAnimator(currentMesh, targetValues));
 
+        float deltaSpawnTime = 1 / ringSpawnRate;
+        WaitForSeconds waiter = new WaitForSeconds(deltaSpawnTime);
+        float fillRate = deltaSpawnTime * fillRateY;
+        while (true)
+        {
+            CreateRing(currentMesh,targetValues,currentUvPosition);
+            currentUvPosition += uvMapSpacing; // move uv up. uv will repeat itself
+            _currentY += fillRate;
+            yield return waiter;
+        }
+    }
+
+    private void CreateRing(Mesh currentMesh,Dictionary<int,Vector3> targetValues,float currentUvPosition)
+    {
         // a strip looks like this
         // number of vertices in ring = 3
                 
@@ -49,74 +68,77 @@ public class IceCreamCreator : MonoBehaviour
         // two triangles of this quad are [4,1,0] and [4,5,1] (creating clockwise is important)
         // Note: in cylinder first and last lies in same position and are not connected. not filling gap because it causes issue while texturing
         
-        WaitForSeconds waiter = new WaitForSeconds(1/ringSpawnRate);
-        while (true)
+        var createPoint = createPosition.position;
+        int startingVertexIndex = currentMesh.vertices.Length;
+        Quaternion rotation = Quaternion.AngleAxis(90, createPoint - center.position);// rotating axis when calculating landing position
+
+        // NOTE: +1 vertices because combining last vertices to first vertex in unity causes texture to wrap backward
+        // so we create extra vertex that is also start
+        Vector3[] newPoints = new Vector3[startingVertexIndex + ringVertices + 1];
+        Vector3[] newNormals = new Vector3[startingVertexIndex + ringVertices + 1];
+        Vector2[] newUv = new Vector2[startingVertexIndex + ringVertices + 1];
+        Array.Copy(currentMesh.vertices, newPoints, startingVertexIndex);
+        Array.Copy(currentMesh.normals, newNormals, startingVertexIndex);
+        Array.Copy(currentMesh.uv, newUv, startingVertexIndex);
+
+        // create points
+        for (int i = 0; i < ringVertices + 1; i++)
         {
-            var createPoint = createPosition.position;
-            int startingVertexIndex = currentMesh.vertices.Length;
-            
-            // NOTE: +1 vertices because combining last vertices to first vertex in unity causes texture to wrap backward
-            // so we create extra vertex that is also start
-            Vector3[] newPoints = new Vector3[startingVertexIndex+ringVertices+1];
-            Vector3[] newNormals = new Vector3[startingVertexIndex+ringVertices+1];
-            Vector2[] newUv = new Vector2[startingVertexIndex+ringVertices+1];
-            Array.Copy(currentMesh.vertices,newPoints,startingVertexIndex);
-            Array.Copy(currentMesh.normals,newNormals,startingVertexIndex);
-            Array.Copy(currentMesh.uv,newUv,startingVertexIndex);
+            float factor = i / (float)ringVertices;
+            float angle = factor * MathF.PI * 2;
+            Vector3 point = new Vector3(ringRadius * Mathf.Cos(angle) + createPoint.x, createPoint.y,
+                ringRadius * Mathf.Sin(angle) + createPoint.z);
+            newPoints[startingVertexIndex + i] = point;
+            newNormals[startingVertexIndex + i] =
+                point - createPoint; // normals are pointing from center to created point
+            newUv[startingVertexIndex + i] = new Vector2(factor / 2, currentUvPosition);
 
-            // create points
-            for (int i = 0; i < ringVertices+1; i++)
-            {
-                float factor = i / (float)ringVertices;
-                float angle = factor* MathF.PI*2;
-                Vector3 point = new Vector3(ringRadius * Mathf.Cos(angle) + createPoint.x, createPoint.y,
-                    ringRadius * Mathf.Sin(angle) + createPoint.z);
-                newPoints[startingVertexIndex + i] = point;
-                newNormals[startingVertexIndex + i] = point - createPoint;// normals are pointing from center to created point
-                newUv[startingVertexIndex + i] = new Vector2(factor / 2, currentUvPosition);
-                
-                // set end point
-                targetValues.Add(startingVertexIndex+i,createPoint-Vector3.up*.4f);
-            }
-            currentUvPosition += uvMapSpacing;// move uv up. uv will repeat itself
-            currentMesh.vertices = newPoints;
-            currentMesh.normals = newNormals;
-            currentMesh.uv = newUv;
-
-            if (startingVertexIndex==0)
-            {
-                // if first point don't create triangle
-                // TODO: close starting point
-                yield return waiter;
-                continue;
-            }
-
-            // create triangles
-            int startingTriangleIndex = currentMesh.triangles.Length;
-            int[] newTriangles = new int[startingTriangleIndex + (ringVertices) * 2*3];// each new vertex creates two triangle and a triangle needs 3 index.
-            Array.Copy(currentMesh.triangles,newTriangles,startingTriangleIndex);
-
-            for (int i = 0; i < ringVertices; i++)
-            {
-                int[] quadPoints = new int[4];
-                int pointIndex = startingVertexIndex + i;
-
-                quadPoints[0] = pointIndex;
-                quadPoints[1] = pointIndex+1;
-                quadPoints[2] = pointIndex-ringVertices;
-                quadPoints[3] = pointIndex-ringVertices-1;
-
-                newTriangles[startingTriangleIndex + i * 6] = quadPoints[0];
-                newTriangles[startingTriangleIndex + i * 6+1] = quadPoints[2];
-                newTriangles[startingTriangleIndex + i * 6+2] = quadPoints[3];
-                newTriangles[startingTriangleIndex + i * 6+3] = quadPoints[0];
-                newTriangles[startingTriangleIndex + i * 6+4] = quadPoints[1];
-                newTriangles[startingTriangleIndex + i * 6+5] = quadPoints[2];
-            }
-
-            currentMesh.triangles = newTriangles;
-            yield return waiter;
+            // set end point
+            var centerPosition = center.position;
+            centerPosition.y = _currentY;// because we have custom landing point
+            Vector3 pointLand = point;
+            pointLand.y = _currentY;
+            Vector3 endPosition = rotation * (pointLand-centerPosition)+centerPosition;// transform to center, rotate and transform back
+            targetValues.Add(startingVertexIndex + i, endPosition);
         }
+
+        currentMesh.vertices = newPoints;
+        currentMesh.normals = newNormals;
+        currentMesh.uv = newUv;
+
+        if (startingVertexIndex == 0)
+        {
+            // if first point don't create triangle
+            // TODO: close starting point
+            return;
+        }
+
+        // create triangles
+        int startingTriangleIndex = currentMesh.triangles.Length;
+        int[] newTriangles =
+            new int[startingTriangleIndex +
+                    (ringVertices) * 2 * 3]; // each new vertex creates two triangle and a triangle needs 3 index.
+        Array.Copy(currentMesh.triangles, newTriangles, startingTriangleIndex);
+
+        for (int i = 0; i < ringVertices; i++)
+        {
+            int[] quadPoints = new int[4];
+            int pointIndex = startingVertexIndex + i;
+
+            quadPoints[0] = pointIndex;
+            quadPoints[1] = pointIndex + 1;
+            quadPoints[2] = pointIndex - ringVertices;
+            quadPoints[3] = pointIndex - ringVertices - 1;
+
+            newTriangles[startingTriangleIndex + i * 6] = quadPoints[0];
+            newTriangles[startingTriangleIndex + i * 6 + 1] = quadPoints[2];
+            newTriangles[startingTriangleIndex + i * 6 + 2] = quadPoints[3];
+            newTriangles[startingTriangleIndex + i * 6 + 3] = quadPoints[0];
+            newTriangles[startingTriangleIndex + i * 6 + 4] = quadPoints[1];
+            newTriangles[startingTriangleIndex + i * 6 + 5] = quadPoints[2];
+        }
+
+        currentMesh.triangles = newTriangles;
     }
 
     IEnumerator CreamAnimator(Mesh movingMesh, Dictionary<int,Vector3> indexToTarget)
